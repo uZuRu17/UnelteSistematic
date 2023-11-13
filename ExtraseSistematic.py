f598ef38179553de92e1b41c05127b780fae58ed
uZuRu17 <parradise_kiss@yahoo.com>
2023-11-13 13:02:17 +0200

    Create ExtraseSistematic.py
    Prima Versiune de Impartit PrintJoburile de la Sistematic

---
+++ ExtraseSistematic.py
@@ -0,0 +1,82 @@
+import fitz  # PyMuPDF
+import re
+import os
+from concurrent.futures import ThreadPoolExecutor
+
+# Create folders if they don't exist
+folders = ['ExtracPlan', 'ExtrasCF']
+for folder in folders:
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+def get_page_count(pdf_path):
+    pdf_document = fitz.open(pdf_path)
+    page_count = pdf_document.page_count
+    pdf_document.close()
+    return page_count
+
+def split_pdf_text(file_path, page_number):
+    array_temporary = [[],[],[]]
+    pdf_document = fitz.open(file_path)
+    first_page = pdf_document[page_number]
+    text = first_page.get_text()
+
+
+    findIE = re.search(r'\d{5,6}', text)
+    if findIE:
+        number = int(findIE.group())
+        array_temporary[1]=number
+    findPagini = re.search(r'\nPagina 1 din (\d+)\n', text)
+    if findPagini:
+        last_number = int(findPagini.group(1)[::-1])
+        array_temporary[2]=last_number
+
+
+    new_pdf = fitz.open()
+    if text[:14] == 'Extras de Plan':
+        array_temporary[0]="Plan_Cadastral"
+        for page_number in range(page_number, page_number + array_temporary[2]):
+            new_pdf.insert_pdf(pdf_document, from_page=page_number, to_page=page_number)
+        output_pdf_path = os.path.join(array_temporary[0], f"{array_temporary[1]}.pdf")
+        #print(output_pdf_path)
+        new_pdf.save(output_pdf_path)
+        new_pdf.close()
+    elif len(text) > 10:
+        array_temporary[0] = "Carte_Funciara"
+        for page_number in range(page_number, page_number + array_temporary[2]):
+            new_pdf.insert_pdf(pdf_document, from_page=page_number, to_page=page_number)
+        output_pdf_path = os.path.join(array_temporary[0], f"{array_temporary[1]}.pdf")
+        #print(output_pdf_path)
+        new_pdf.save(output_pdf_path)
+        new_pdf.close()
+    else:
+        array_temporary[0] = "Blank"
+        return(array_temporary)
+
+    pdf_document.close()
+    return(array_temporary)
+def process_pages_with_condition(pdf_path, start_page=0):
+
+    #page_number = start_page
+    page_count = get_page_count(pdf_path)
+    while start_page < page_count:
+        textx = split_pdf_text(pdf_path, start_page)
+        if textx[0] == "Blank":
+            start_page += 1
+        else:
+            start_page += textx[-1]
+
+
+def multisplit_pdfs_in_folder(folder_path):
+    pdf_files = [file for file in os.listdir(folder_path) if file.lower().endswith((".pdf", ".PDF"))]
+
+    with ThreadPoolExecutor() as executor:
+        print(pdf_files)
+        executor.map(process_pages_with_condition, pdf_files)
+
+
+# Example usage:
+folder_path = r'C:\Users\user\Documents\DEV\SISCAD'
+pdf_files = multisplit_pdfs_in_folder(folder_path)
+
+#do_something = process_pages_with_condition(folder_path+r"\PrintJob.10005238860.PDF")
